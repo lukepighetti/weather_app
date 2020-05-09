a04c35a41c6d89a0252ddf19c020499115bcc757
lukepighetti <lukepighetti@gmail.com>
2020-05-09 15:24:30 -0400

    wire up states_rebuilder on WeatherScreen

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:srl/services/weather_api.dart';
 import 'package:srl/services/weather_api_models.dart';
 import 'package:srl/extensions/double.dart';
+import 'package:states_rebuilder/states_rebuilder.dart';
 
 class WeatherScreen extends StatefulWidget {
   @override
@@ -11,25 +12,25 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   WeatherService _weatherService = WeatherService.instance;
 
+  final ReactiveModel<OpenWeatherOneCall> weatherOneCall =
+      ReactiveModel.create(null);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<OpenWeatherOneCall>(
-        future: _weatherService.fetchWeather(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            final weatherCall = snapshot.data;
-            return Center(
-              child: Text(
-                '${weatherCall.current.temp.asFahrenheit.toStringAsPrecision(3)}',
-              ),
-            );
-          }
-        },
+      body: Center(
+        child: WhenRebuilder<OpenWeatherOneCall>(
+          observe: () => weatherOneCall,
+          initState: (context, reactiveModel) => reactiveModel.future(
+            (initialValue) => _weatherService.fetchWeather(),
+          ),
+          onIdle: () => Text("Waiting for weather..."),
+          onWaiting: () => CircularProgressIndicator(),
+          onError: (e) => Text(e),
+          onData: (OpenWeatherOneCall weatherCall) => Text(
+            '${weatherCall.current.temp.asFahrenheit.toStringAsPrecision(3)}',
+          ),
+        ),
       ),
     );
   }
