1f22df5616084f25ac14d5aaadafe5f0fb405208
lukepighetti <lukepighetti@gmail.com>
2020-05-09 14:37:14 -0400

    generate models for weather api
    Co-authored-by: GroovinChip <groovinchip@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,18 @@ class SRWeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'StatesRebuilder Weather',
+      title: 'SR Weather',
       theme: ThemeData(
         colorScheme: ColorScheme.light().copyWith(
+          primary: Color(0xff2962FF),
+          primaryVariant: Color(0xff0039cb),
+          secondary: Color(0xff3BACFF),
+          secondaryVariant: Color(0xff007ecb),
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.dark().copyWith(
           primary: Color(0xff2962FF),
           primaryVariant: Color(0xff0039cb),
           secondary: Color(0xff3BACFF),
---
+++ lib/services/weather_api_models.g.dart
@@ -0,0 +1,68 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_api_models.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+OpenWeatherOneCall _$OpenWeatherOneCallFromJson(Map<String, dynamic> json) {
+  return OpenWeatherOneCall(
+    (json['lat'] as num)?.toDouble(),
+    (json['lon'] as num)?.toDouble(),
+    json['timezone'] as String,
+    json['current'] == null
+        ? null
+        : OpenWeatherCurrent.fromJson(json['current'] as Map<String, dynamic>),
+    (json['hourly'] as List)
+        ?.map((e) => e == null
+            ? null
+            : OpenWeatherHourlyPoint.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
+}
+
+OpenWeatherHourlyPoint _$OpenWeatherHourlyPointFromJson(
+    Map<String, dynamic> json) {
+  return OpenWeatherHourlyPoint(
+    _openWeatherDateTimeFrom(json['dt'] as int),
+    (json['temp'] as num)?.toDouble(),
+    (json['feels_like'] as num)?.toDouble(),
+    (json['pressure'] as num)?.toDouble(),
+    (json['humidity'] as num)?.toDouble(),
+    (json['dew_point'] as num)?.toDouble(),
+    (json['clouds'] as num)?.toDouble(),
+    (json['wind_speed'] as num)?.toDouble(),
+    (json['wind_deg'] as num)?.toDouble(),
+    _openWeatherDescriptionFrom(json['weather'] as List),
+  );
+}
+
+OpenWeatherCurrent _$OpenWeatherCurrentFromJson(Map<String, dynamic> json) {
+  return OpenWeatherCurrent(
+    _openWeatherDateTimeFrom(json['dt'] as int),
+    _openWeatherDateTimeFrom(json['sunrise'] as int),
+    _openWeatherDateTimeFrom(json['sunset'] as int),
+    (json['temp'] as num)?.toDouble(),
+    (json['feels_like'] as num)?.toDouble(),
+    (json['pressure'] as num)?.toDouble(),
+    (json['humidity'] as num)?.toDouble(),
+    (json['dew_point'] as num)?.toDouble(),
+    (json['uvi'] as num)?.toDouble(),
+    (json['clouds'] as num)?.toDouble(),
+    (json['visibility'] as num)?.toDouble(),
+    (json['wind_speed'] as num)?.toDouble(),
+    (json['wind_deg'] as num)?.toDouble(),
+    _openWeatherDescriptionFrom(json['weather'] as List),
+  );
+}
+
+OpenWeatherDescription _$OpenWeatherDescriptionFromJson(
+    Map<String, dynamic> json) {
+  return OpenWeatherDescription(
+    json['id'] as int,
+    json['main'] as String,
+    json['description'] as String,
+    json['icon'] as String,
+  );
+}
